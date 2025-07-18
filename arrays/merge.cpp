09179792b19d32fb2ae19af3d7b4c2f5382433f9
chirag <sharmachirag242004@gmail.com>
2025-07-18 12:46:14 +0530

    new commit

---
+++ arrays/merge.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int mergeOp(vector<int> &arr) {
+    int start = 0;
+    int end = arr.size() - 1;
+    int count = 0;
+
+    while (start < end) {
+        if(arr[start] == arr[end]) {
+            start++;
+            end--;
+        } else if(arr[start] < arr[end]) {
+            arr[start + 1] = arr[start + 1] + arr[start];
+            start++;
+            count++;
+        } else {
+            arr[end - 1] = arr[end - 1] + arr[end];
+            end--;
+            count++;
+        }
+    }
+    return count;
+}
+int main() {
+    vector<int> arr = {6, 1, 4, 3, 1, 7};
+    int ans = mergeOp(arr);
+    cout << "operations are: " << ans << endl;
+    return 0;
+}
