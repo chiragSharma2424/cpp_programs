96964f4f797366dcead5aa088a849b7d14757569
chirag <sharmachirag242004@gmail.com>
2025-07-21 15:39:14 +0530

    new commit

---
+++ clg/map.js
@@ -0,0 +1,6 @@
+let arr = [1, 2, 3, 4];
+let ans = arr.map((ele) => {
+    console.log(ele * 2);
+    return ele * 2;
+});
+console.log(ans);
--- clg/test.cpp
+++ clg/test.cpp
@@ -1,12 +1,23 @@
 #include<iostream>
+#include<string>
 using namespace std;
+int countSpaces(string str) {
+    int count = 0;
+    for(int i = 0; i < str.length(); i++) {
+        if(str[i] == ' ' && str[i+1] != ' ') count++;
+    }
+    return count;
+}
+
 int main() {
-    int rnum;
-    string name;
-    cin >> rnum;
-    fflush(stdin);
-    getline(cin, name);
-    cout << rnum;
-    cout << name;
+    // int rnum;
+    // string name;
+    // cin >> rnum;
+    // fflush(stdin);
+    // getline(cin, name);
+    // cout << rnum;
+    // cout << name;
+    int ans = countSpaces("hello world from       coders");
+    cout << ans << endl;
     return 0;
 }
