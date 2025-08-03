1d14ca4bf79e5e3a5956e3c89fa04fc0e65587b2
chirag <sharmachirag242004@gmail.com>
2025-08-03 15:35:04 +0530

    new commit

--- clg/map.js
+++ clg/map.js
@@ -1,6 +1,28 @@
-let arr = [1, 2, 3, 4];
-let ans = arr.map((ele) => {
-    console.log(ele * 2);
-    return ele * 2;
-});
-console.log(ans);
+// let arr = [1, 2, 3, 4];
+// let ans = arr.map((ele) => {
+//     console.log(ele * 2);
+//     return ele * 2;
+// });
+// console.log(ans);
+
+
+const crypto = require('crypto');
+
+function hashWithPrefix(prefix) {
+    let input = 596139;
+    while(true) {
+        let inputStr = "chirag" + input.toString();
+        let hash = crypto.createHash('sha256').update(inputStr).digest('hex');
+        if(hash.startsWith(prefix)) {
+            return {
+                input: inputStr,
+                hash: hash
+            }
+        }
+        input++;
+    }
+}
+
+const result = hashWithPrefix('0000');
+console.log(`input: ${result.input}`);
+console.log(`hash: ${result.hash}`);
--- linkedList/practice.cpp
+++ linkedList/practice.cpp
@@ -23,6 +23,24 @@ Node *array2ll(vector<int> &arr) {
     return head;
 }
 
+Node *array2ll2(vector<int> &arr) {
+    Node *head = new Node(arr[0]);
+    Node *mover = head;
+
+    for(int i = 1; i < arr.size(); i++) {
+       Node *newNode = new Node(arr[i]);
+       mover -> next = newNode;
+       mover = mover -> next;
+    }
+    return head;
+}
+
+Node *insertAtHead(Node *head, int data) {
+    Node *newHead = new Node(data);
+    newHead -> next = head;
+    return newHead;
+}
+
 void printLL(Node *head) {
     Node *temp = head;
     while(temp) {
@@ -33,7 +51,9 @@ void printLL(Node *head) {
 }
 int main() {
     vector<int> arr = {24, 4, 5, 9, 3};
-    Node *head = array2ll(arr);
+    Node *head = array2ll2(arr);
+    printLL(head);
+    head = insertAtHead(head, 2004);
     printLL(head);
     return 0;
 }
